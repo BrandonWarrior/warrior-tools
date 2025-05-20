66ab8c8b801b475e821187a58c87c4d8a31541e3
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-05-20 19:09:08 +0100

    added add to bag functionaility

--- products/views.py
+++ products/views.py
@@ -5,12 +5,8 @@ from django.db.models.functions import Lower
 
 from .models import Product, Category
 
-
-# Create your views here.
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
     query = None
     categories = None
@@ -24,10 +20,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -44,7 +38,6 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -62,7 +55,6 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
-
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
