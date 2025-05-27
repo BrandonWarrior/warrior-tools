bd9d51b110b37498e6fce9f85650f9d2c40554d5
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-05-27 19:07:40 +0100

    add delete products

--- products/views.py
+++ products/views.py
@@ -6,41 +6,6 @@ from django.db.models.functions import Lower
 from .models import Product, Category
 from .forms import ProductForm
 
-import json  # To pass variant list into JS
-
-# Variant types that use size selection
-variant_options = [
-    'hammer',
-    'sds_drill',
-    'drill_driver',
-    'impact_driver',
-    'jigsaw',
-    'circular_saw',
-    'orbital_sander',
-    'tape_measure',
-    'hand_saw',
-]
-
-# Tool types by category logic
-HAND_TOOL_VARIANTS = [
-    'hammer',
-    'pliers',
-    'tape_measure',
-    'hand_saw',
-    'pipe_cutter',
-    'wood_chisel',
-    'spirit_level',
-]
-
-POWER_TOOL_VARIANTS = [
-    'sds_drill',
-    'drill_driver',
-    'impact_driver',
-    'jigsaw',
-    'circular_saw',
-    'orbital_sander',
-]
-
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
@@ -63,31 +28,11 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
-            category_filters = request.GET['category'].split(',')
-
-            # Base category filter
-            product_filter = Q(category__name__in=category_filters)
-
-            expanded_categories = category_filters.copy()
-
-            if 'hand_tools' in category_filters:
-                product_filter |= Q(
-                    category__name='new_arrivals',
-                    variant_type__in=HAND_TOOL_VARIANTS
-                )
-                expanded_categories.append('new_arrivals')
-
-            if 'power_tools' in category_filters:
-                product_filter |= Q(
-                    category__name='new_arrivals',
-                    variant_type__in=POWER_TOOL_VARIANTS
-                )
-                expanded_categories.append('new_arrivals')
-
-            products = products.filter(product_filter)
-            categories = Category.objects.filter(name__in=expanded_categories)
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -112,12 +57,9 @@ def all_products(request):
 def product_detail(request, product_id):
     """ A view to show individual product details """
     product = get_object_or_404(Product, pk=product_id)
-
     context = {
         'product': product,
-        'variant_options': variant_options,
     }
-
     return render(request, 'products/product_detail.html', context)
 
 
@@ -126,19 +68,15 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-
-    context = {
-        'form': form,
-        'variant_options': json.dumps(variant_options),
-    }
-
+        
+    context = {'form': form}
     return render(request, 'products/add_product.html', context)
 
 
@@ -160,7 +98,13 @@ def edit_product(request, product_id):
     context = {
         'form': form,
         'product': product,
-        'variant_options': json.dumps(variant_options),
     }
-
     return render(request, 'products/edit_product.html', context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
