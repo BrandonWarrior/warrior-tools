25d603706704ef7cc95458b95b54d15fc4968c8a
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-05-29 18:50:35 +0100

    further deployment setup

--- warrior_tools/settings.py
+++ warrior_tools/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
