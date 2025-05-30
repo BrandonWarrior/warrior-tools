5e4defcd0cc74cbe7b92c18f8f0c371978a487ca
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-05-30 23:42:34 +0100

    fix email config block structure

--- warrior_tools/settings.py
+++ warrior_tools/settings.py
@@ -231,6 +231,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+# Email configuration
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'warriortools@example.com'
