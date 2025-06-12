5d238c1385ce089aac56ecda98cd021a05b29214
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-06-12 19:58:08 +0100

    remove global email backend override

--- warrior_tools/settings.py
+++ warrior_tools/settings.py
@@ -235,7 +235,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Email configuration
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+   # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'warriortools@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
