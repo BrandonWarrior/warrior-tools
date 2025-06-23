6d17c86457debdafa1908aa3f687e838678a00d4
Brandon Warrior <brandonwarrior@MacBookPro.lan>
2025-06-23 11:47:02 +0100

    Fix product_size field length and syntax error in OrderLineItem model

--- checkout/models.py
+++ checkout/models.py
@@ -87,7 +87,7 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False,
         on_delete=models.CASCADE)
     product_size = models.CharField(
-        max_length=2, null=True, blank=True)  # XS, S, M, L, XL
+        max_length=20, null=True, blank=True)  # Increased from 2 to 20
     quantity = models.IntegerField(
         null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
